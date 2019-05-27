6d40489c8093f0bfc1aa7331006e22c2f03f5b25
zhaoyongsheng <zhaoyongsheng@youxin.com>
2019-05-27 11:07:45 +0800

    build: 初始化项目

---
+++ service.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
